15b55bf7a4aedd990bb59bff57bfb6528f69a7af
Miguel Liezun <liezun.js@gmail.com>
2018-02-03 14:21:15 -0300

    Removed consts from function and moved clearTimeout before emit events

--- clientapp.js
+++ clientapp.js
@@ -1,30 +1,30 @@
 /* http://www.iana.org/assignments/multicast-addresses */
 
 const dgram = require('dgram')
-const client = dgram.createSocket('udp4')
-const EventEmitter = require('events')
 const crypto = require('crypto')
+const EventEmitter = require('events')
 
 class MyEmitter extends EventEmitter {}
 
 module.exports.getServerAddress = (key, logger) => {
-  const cipher = crypto.createCipher('aes-256-cbc', key)
-  const random = Math.random().toString()
+  let client = dgram.createSocket('udp4')
+  let cipher = crypto.createCipher('aes-256-cbc', key)
+  let random = Math.random().toString()
                 + Math.random().toString()
                 + Math.random().toString()
                 + Math.random().toString()
-  const message = Buffer.concat([cipher.update(new Buffer(random, 'utf8')), cipher.final()])
+  let message = Buffer.concat([cipher.update(new Buffer(random, 'utf8')), cipher.final()])
 
   let timeoutId
   let found = false
-  const result = new MyEmitter()
+  let result = new MyEmitter()
 
   if (!logger) logger = console.log
 
   client.on('error', (err) => {
-    logger(`client error:\n${err.stack}`)
     clearTimeout(timeoutId)
     client.close()
+    logger(`client error:\n${err.stack}`)
   })
 
   client.on('message', (msg, rinfo) => {
@@ -38,32 +38,32 @@ module.exports.getServerAddress = (key, logger) => {
         found = true
         result.emit('address', rinfo.address)
       } else {
-        logger(`KeyError: ${decrypted} from ${rinfo.address}:${rinfo.port}`)
-        result.emit('error', 'KeyError')
         clearTimeout(timeoutId)
         client.close()
+        logger(`KeyError: ${decrypted} from ${rinfo.address}:${rinfo.port}`)
+        result.emit('error', 'KeyError')
       }
     } catch (e) {
-      logger(e)
-      result.emit('error', 'KeyError')
       clearTimeout(timeoutId)
       client.close()
+      logger(e)
+      result.emit('error', 'KeyError')
     }
   })
 
   client.on('listening', () => {
-    const address = client.address()
+    let address = client.address()
     logger(`client listening ${address.address}:${address.port}`)
   })
 
-  client.bind(55555, () => {
+  client.bind(() => {
     client.send(message, 44444, '239.255.0.1')
     timeoutId = setTimeout(() => {
+      client.close()
       if (!found) {
         logger(`NotFound`)
         result.emit('error', 'NotFound')
       }
-      client.close()
     }, 1000)
   })
 
